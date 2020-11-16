f2f0b9032cea699f682cff4fe146d6f6c4e6a5b4
seeris <sergei.udris@gmail.com>
2020-11-16 09:10:07 +0300

    use /src (keep it flat) for experiments; add /src/jvm-libp2p

---
+++ src/jvm-libp2p/src/jvm-libp2p/chatnode.clj
@@ -0,0 +1,7 @@
+(ns jvm-libp2p.chatnode
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [clojure.string :as str]))
