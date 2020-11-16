eb09c67cb6e4ded8ab07d776f8f16ae3651493c8
seeris <sergei.udris@gmail.com>
2020-11-16 14:36:41 +0300

    add /src/js-libp2p

---
+++ src/js-libp2p/src/lab/js-libp2p/discovery.cljs
@@ -0,0 +1,19 @@
+(ns lab.js-libp2p.discovery
+  (:require
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [goog.string :refer [format]]
+   [goog.object]
+   [clojure.string :as string]
+   [cljs.reader :refer [read-string]]
+   [cljs.nodejs :as node]))
+
+(def fs (node/require "fs"))
+(def path (node/require "path"))
+
+
+(defn main []
+  (println "hello"))
