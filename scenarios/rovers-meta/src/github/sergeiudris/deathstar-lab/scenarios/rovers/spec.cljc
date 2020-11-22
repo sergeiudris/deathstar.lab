9b54717475da73fada4def276a948f4d8baf7c37
seeris <sergei.udris@gmail.com>
2020-11-22 14:25:33 +0300

    add  sceanrios/rovers-meta, build rovers

---
+++ scenarios/rovers-meta/src/github/sergeiudris/deathstar-lab/scenarios/rovers/spec.cljc
@@ -0,0 +1,6 @@
+(ns github.sergeiudris.deathstar-lab.scenarios.rovers.spec
+  #?(:cljs (:require-macros [github.sergeiudris.deathstar-lab.scenarios.rovers.spec]))
+  (:require
+   [clojure.spec.alpha :as s]))
+
+(s/def ::rover some?)
--- scenarios/rovers/src/github/sergeiudris/deathstar_lab/scenarios/rovers/main.cljs
+++ scenarios/rovers/src/github/sergeiudris/deathstar_lab/scenarios/rovers/main.cljs
@@ -1,4 +1,78 @@
-(ns github.sergeiudris.deathstar-lab.scenarios.rovers.render
+(ns github.sergeiudris.deathstar-lab.scenarios.rovers.main
   (:require
-   ))
+   [clojure.core.async :as a :refer [chan go go-loop <! >!  take! put! offer! poll! alt! alts! close!
+                                     pub sub unsub mult tap untap mix admix unmix pipe
+                                     timeout to-chan  sliding-buffer dropping-buffer
+                                     pipeline pipeline-async]]
+   [cljs.core.async.impl.protocols :refer [closed?]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [goog.string.format :as format]
+   [goog.string :refer [format]]
+   [goog.object]
+   [clojure.string :as string]
+   [cljs.reader :refer [read-string]]
 
+   [cljctools.csp.op.spec :as op.spec]
+   [cljctools.cljc.core :as cljc.core]
+
+   [cljctools.rsocket.spec :as rsocket.spec]
+   [cljctools.rsocket.chan :as rsocket.chan]
+   [cljctools.rsocket.impl :as rsocket.impl]
+
+   [deathstar.app.spec :as app.spec]
+   [deathstar.app.chan :as app.chan]
+
+   [github.sergeiudris.deathstar-lab.scenarios.rovers.spec :as rovers.spec]
+   [github.sergeiudris.deathstar-lab.scenarios.rovers.chan :as rovers.chan]
+   [github.sergeiudris.deathstar-lab.scenarios.rovers.render :as rovers.render]))
+
+(goog-define RSOCKET_PORT 0)
+
+(def channels (merge
+               (app.chan/create-channels)
+               (rovers.chan/create-channels)
+               (rsocket.chan/create-channels)))
+
+(pipe (::rsocket.chan/requests| channels) (::rovers.chan/ops| channels))
+
+(pipe (::app.chan/ops| channels) (::rsocket.chan/ops| channels))
+
+(def state (rovers.render/create-state
+            {}))
+
+(defn create-proc-ops
+  [channels opts]
+  (let [{:keys [::rovers.chan/ops|]} channels]
+    (go
+      (loop []
+        (when-let [[value port] (alts! [ops|])]
+          (condp = port
+            ops|
+            (condp = (select-keys value [::op.spec/op-key ::op.spec/op-type ::op.spec/op-orient])
+
+              {::op.spec/op-key ::rovers.chan/init}
+              (let [{:keys []} value]
+                (println ::init)
+                (rovers.render/render-ui channels state {})))))
+        (recur)))))
+
+#_(def rsocket (rsocket.impl/create-proc-ops
+                channels
+                {::rsocket.spec/connection-side ::rsocket.spec/initiating
+                 ::rsocket.spec/host "localhost"
+                 ::rsocket.spec/port RSOCKET_PORT
+                 ::rsocket.spec/transport ::rsocket.spec/websocket}))
+
+(def rovers (create-proc-ops channels {}))
+
+(defn ^:export main
+  []
+  (println ::main)
+  (println ::RSOCKET_PORT RSOCKET_PORT)
+  (rovers.chan/op
+   {::op.spec/op-key ::rovers.chan/init}
+   channels
+   {}))
+
+
+(do (main))
