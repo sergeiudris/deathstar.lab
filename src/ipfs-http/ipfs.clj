56e4e2ad14cca352bcdec8e0be9d16e660791c1e
Sergei Udris <sergei.udris@gmail.com>
2021-04-21 08:47:56 +0300

    src/dgraph-http src/ipfs-http

--- src/ipfs-http/ipfs.clj
+++ src/ipfs-http/ipfs.clj
@@ -1,10 +1,11 @@
-(ns deathstar.peer.ipfs
+(ns find.app.ipfs
   (:require
    [clojure.core.async :as a :refer [chan go go-loop <! >! <!! >!!  take! put! offer! poll! alt! alts! close!
                                      pub sub unsub mult tap untap mix admix unmix pipe
                                      timeout to-chan  sliding-buffer dropping-buffer
                                      pipeline pipeline-async]]
    [clojure.string]
+   [clojure.pprint :refer [pprint]]
    [clojure.spec.alpha :as s]
    [clojure.java.io :as io]
    [clj-http.client :as clj-http.client]
@@ -14,7 +15,7 @@
 
 (s/def ::pubsub-topic string?)
 
-(defn version
+(defn api-version
   []
   (go
     (let [response
@@ -27,7 +28,7 @@
            (jsonista.core/read-value jsonista.core/keyword-keys-object-mapper))]
       response)))
 
-(defn pubsub-sub
+(defn api-pubsub-sub
   [{:keys [::pubsub-topic] :or {pubsub-topic "123"}}]
   (go
     (with-open [stream (->
@@ -41,3 +42,65 @@
         (doseq [line lines]
           (println :line
                    (jsonista.core/read-value line jsonista.core/keyword-keys-object-mapper)))))))
+
+(defn api-swarm-peers
+  []
+  (go
+    (let [response
+          (->
+           (clj-http.client/request
+            {:url (str base-url "/api/v0/swarm/peers")
+             :method :post
+             :headers {:content-type "application/json"}
+             :query-params {"verbose" true
+                            "streams" true
+                            "latency" true
+                            "direction" true}})
+           :body
+           (jsonista.core/read-value jsonista.core/keyword-keys-object-mapper))]
+      (->>
+       response
+       :Peers
+       #_(map :Peer)
+       rand-nth
+       #_count
+       #_pprint))))
+
+(defn api-find-peer
+  []
+  (go
+    (let [peer-id (:Peer (<! (api-swarm-peers)))
+          response
+          (->
+           (clj-http.client/request
+            {:url (str base-url "/api/v0/dht/findpeer")
+             :method :post
+             :headers {:content-type "application/json"}
+             :query-params {"arg" peer-id
+                            "verbose" true}})
+           :body
+           (jsonista.core/read-value jsonista.core/keyword-keys-object-mapper))]
+      response)))
+
+(defn api-name-resolve
+  []
+  (go
+    (let [peer-id (:Peer (<! (api-swarm-peers)))
+          response
+          (->
+           (clj-http.client/request
+            {:url (str base-url "/api/v0/name/resolve")
+             :method :post
+             :headers {:content-type "application/json"}
+             :query-params {"arg" peer-id
+                            "dht-record-count" 10
+                            "dht-timeout" "30s"}})
+           :body
+           (jsonista.core/read-value jsonista.core/keyword-keys-object-mapper))]
+      response)))
+
+
+(defn crawl
+  []
+  
+  )
